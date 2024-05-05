7fa290567525b6b86e514583452ff62ef887bcaf
Mus <muslim@MacBook-Air-Mus.local>
2024-05-05 23:28:33 +0300

    06/05

--- src/components/body/homepage/CarPark.jsx
+++ src/components/body/homepage/CarPark.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import style from "../homepage/CarPark.module.css";
 import vector from "../../../image/Vector.svg";
 import auto from "../../../image/auto.svg";
 import auto1 from "../../../image/auto1.svg";
 import auto3 from "../../../image/auto2.svg";
+import calendar from "../../../image/Calendar.svg";
+import gearbox from "../../../image/Gearbox.svg";
+import fuel from "../../../image/fuel.svg";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAuto } from "../../../Features/autoSlice";
 function CarPark() {
+
+const auto  = useSelector((state)=> state.autoSlice.auto)
+
+const dispatch = useDispatch()
+
+useEffect(()=>{
+  dispatch(fetchAuto())
+})
+
   return (
     <>
       <div className={style.CarPark}>
@@ -48,24 +62,37 @@ function CarPark() {
           </div>
         </div>
         <div className={style.auto}>
-          <div className={style.autopos}>
-            <img src={auto} alt="" />
+          {auto.map((item)=>{
+            return (
+          <div key={item.id} className={style.autopos}>
+            <img src={item.image} alt="" />
+            <div className={style.autoname}>
+              <span className={style.name}> {item.brand.title} {item.model}</span>
+              <span className={style.rating}> {item.rating} ★</span>
+            </div>
+            <div className={style.autoconfig}>
+              <div className={style.config}>
+                <img src={calendar} alt="" />
+                <span>{item.year}</span>
+              </div>
+              <div className={style.config}>
+                <img src={gearbox} alt="" />
+                <span>{item.gearBox}</span>
+              </div>
+              <div className={style.config}>
+                <img src={fuel} alt="" />
+                <span>{item.fuel}</span>
+              </div>
+            </div>
+            <div className={style.priceblock}>
+              <span className={style.price}>от {item.price} AED</span>
+              <span className={style.day}> / cутки</span>
+            </div>
           </div>
-          <div className={style.autopos}>
-            <img src={auto1} alt="" />
-          </div>
-          <div className={style.autopos}>
-            <img src={auto3} alt="" />
-          </div>
-          <div className={style.autopos}>
-            <img src={auto} alt="" />
-          </div>
-          <div className={style.autopos}>
-            <img src={auto1} alt="" />
-          </div>
-          <div className={style.autopos}>
-            <img src={auto3} alt="" />
-          </div>
+
+            )
+          })}
+         
         </div>
       </div>
     </>
